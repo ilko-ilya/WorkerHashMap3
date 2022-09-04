f0efaf5f0ed094338ef9a0beac64e4bff5ef02ab
Ilya Samilyak <samilyakilya@gmail.com>
2022-09-04 18:33:54 +0300

    Подсчитать кол-во значений

--- src/main/java/com/ilya/worker_map/lesson3/Nature.java
+++ src/main/java/com/ilya/worker_map/lesson3/Nature.java
@@ -4,24 +4,15 @@ import java.util.Arrays;
 
 public class Nature {
     public static void main(String[] args) {
-        int[] arr = {56, 89, 43, 2, -6, 0, 46, 19};
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + ", ");
-        }
-        System.out.println();
-
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
-
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 1; j < arr.length -i; j++) {
-                if (arr[j] < arr[j -1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j -1];
-                    arr[j -1] = temp;
-                }
-            }
-            System.out.print(arr[i] + ", ");
-        }
+        int i = countSymbols("Jhj,.!lkjlk.,adfa.!");
+        System.out.println(i);
     }
+    public static int countSymbols(String sentence){
+        String replaces = sentence.replace(".", "")
+                .replace(",", "")
+                .replace("!", "");
+        return sentence.length() - replaces.length();
+    }
+
 }
+
