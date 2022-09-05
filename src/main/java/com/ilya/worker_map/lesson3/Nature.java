687a03d2449bdb5e657533f40c1c6fdc9f9930e7
Ilya Samilyak <samilyakilya@gmail.com>
2022-09-05 23:02:48 +0300

    Соединение двух массивов

--- src/main/java/com/ilya/worker_map/lesson3/Nature.java
+++ src/main/java/com/ilya/worker_map/lesson3/Nature.java
@@ -4,15 +4,21 @@ import java.util.Arrays;
 
 public class Nature {
     public static void main(String[] args) {
-        int i = countSymbols("Jhj,.!lkjlk.,adfa.!");
-        System.out.println(i);
-    }
-    public static int countSymbols(String sentence){
-        String replaces = sentence.replace(".", "")
-                .replace(",", "")
-                .replace("!", "");
-        return sentence.length() - replaces.length();
-    }
+        int[] arr = mixArr(new int[]{1, 2, 3}, new int[]{4, 5, 6});
+        System.out.println(Arrays.toString(arr));
 
+
+    }
+    public static int[] mixArr(int[] first, int[] second){
+        int[] newArr = new int[first.length + second.length];
+        int count = 0;
+        for (int i = 0; i < first.length; i++) {
+            newArr[count++] = first[i];
+        }
+        for (int i = 0; i < second.length; i++) {
+            newArr[count++] = second[i];
+        }
+        return newArr;
+    }
 }
 
