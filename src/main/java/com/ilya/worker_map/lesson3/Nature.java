71b7ee32d6c6b6983748b7dc441b91faa10f253d
Ilya Samilyak <samilyakilya@gmail.com>
2022-09-06 22:32:26 +0300

    MinMax

--- src/main/java/com/ilya/worker_map/lesson3/Nature.java
+++ src/main/java/com/ilya/worker_map/lesson3/Nature.java
@@ -4,21 +4,28 @@ import java.util.Arrays;
 
 public class Nature {
     public static void main(String[] args) {
-        int[] arr = mixArr(new int[]{1, 2, 3}, new int[]{4, 5, 6});
-        System.out.println(Arrays.toString(arr));
+
+        int[] arr = {34, 67, 2, 78, -75, 8, -19, 50, 21};
+
+        int min = arr[0];
+        int max = arr[0];
+        double ave = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if(min > arr[i]){
+                min = arr[i];
+            }
+            if (max < arr[i]){
+                max = arr[i];
+            }
+            ave += arr[i] / arr.length;
+
+        }
+        System.out.println("Min is: " + min);
+        System.out.println("Max is: " + max);
+        System.out.println("Ave is: " + ave);
 
 
-    }
-    public static int[] mixArr(int[] first, int[] second){
-        int[] newArr = new int[first.length + second.length];
-        int count = 0;
-        for (int i = 0; i < first.length; i++) {
-            newArr[count++] = first[i];
-        }
-        for (int i = 0; i < second.length; i++) {
-            newArr[count++] = second[i];
-        }
-        return newArr;
     }
 }
 
